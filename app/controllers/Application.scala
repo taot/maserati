31e9e53ef492dac484f6dda4496fd57cfe3fb4b1
Terry Tao <librakevin@gmail.com>
2014-08-09 18:22:04 +0800

    Change favicon but failed, more style on mobiles page.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,10 @@ import maserati.Util.isMobile
 
 object Application extends Controller {
 
+/*  def favicon = Action {
+    Ok(Assets.at("/public", "images/favicon.ico", true))
+  }*/
+
   def mobiles = Action { implicit request =>
     if (isMobile(request)) {
       Ok(views.html.classic.mobiles("Hello"))

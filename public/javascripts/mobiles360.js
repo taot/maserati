194eb88cd21f8a0a4982c00d3f88ff6f9e0646cc
Terry Tao <librakevin@gmail.com>
2014-08-14 21:32:44 +0800

    Some changes.

--- public/javascripts/mobiles360.js
+++ public/javascripts/mobiles360.js
@@ -39,6 +39,46 @@ function hide360() {
     $('.mobiles-360-container').fadeOut('slow');
 };
 
+function resizeDiscover(container, width, height) {
+    // calculate width and height
+    var w = width;
+    var h = height;
+    if (w * 480 > h * 640) {
+        w = h * 640 / 480;
+    } else {
+        h = w * 480 / 640;
+    }
+    container.css('width', w);
+    container.css('height', h);
+
+    // calculate position
+    var left = (width - w) / 2;
+    container.css('left', left);
+};
+
+function showDiscover(src) {
+    console.log("showing discover page");
+    var container = $('.discover-container');
+
+    var width = $(window).width();
+    var height = $(window).height();
+    resizeDiscover(container, width, height);
+
+    $(window).resize(function() {
+        var width = $(window).width();
+        var height = $(window).height();
+        console.log("window resized - width: " + width + ", height: " + height);
+        var container = $('.discover-container');
+        resizeDiscover(container, width, height);
+    });
+
+    container.fadeIn('fast');
+};
+
+function hideDiscover() {
+    console.log('hiding discover page');
+    $('.discover-container').fadeOut('slow');
+};
 
 $(document).ready(function() {
 
@@ -49,5 +89,14 @@ $(document).ready(function() {
 
     $('.btn.close-360-btn').click(function(event) {
         hide360();
+    });
+
+    $('.discover-more-btn').click(function(event) {
+        var src = event.target.getAttribute('interior');
+        showDiscover("");
+    });
+
+    $('.close-discover-btn').click(function(event) {
+        hideDiscover();
     });
 });

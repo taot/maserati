a770a4537d00625535e3f894714a942941347579
Terry Tao <librakevin@gmail.com>
2014-08-14 22:31:31 +0800

    Added auto show info page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,15 +6,19 @@ import maserati.Util.isMobile
 
 object Application extends Controller {
 
-/*  def favicon = Action {
-    Ok(Assets.at("/public", "images/favicon.ico", true))
-  }*/
-
   def mobiles = Action { implicit request =>
     if (isMobile(request)) {
       Ok(views.html.classic.mobiles("Hello"))
     } else {
       Ok(views.html.classic.mobiles("Hello"))
+    }
+  }
+
+  def autoshows = Action { implicit request =>
+    if (isMobile(request)) {
+      Ok(views.html.classic.show("Hello"))
+    } else {
+      Ok(views.html.classic.show("Hello"))
     }
   }
 }

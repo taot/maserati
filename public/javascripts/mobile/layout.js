20850bafe6cfe861b880c01805d5d1516ec2a231
Terry Tao <librakevin@gmail.com>
2014-08-18 22:07:57 +0800

    Some changes to mobile

--- public/javascripts/mobile/layout.js
+++ public/javascripts/mobile/layout.js
@@ -1,6 +1,19 @@
+function toggleMenuBar(event) {
+    var display = $('.menu-bar').css('display');
+    if (display == 'none') {
+        $('.menu-bar').fadeIn('slow', function() {
+            $('.menu-btn').css('background-image', 'url(/assets/images/menu_main_close.jpg)');
+        });
+    } else {
+        $('.menu-bar').fadeOut('slow', function() {
+            $('.menu-btn').css('background-image', 'url(/assets/images/menu_main.jpg)');
+        });
+    }
+};
+
 $(document).ready(function() {
 
     $('.menu-btn').click(function(event) {
-        $('.menu-bar').toggle();
+        toggleMenuBar(event);
     });
 });

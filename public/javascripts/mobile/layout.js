e889eaffce9cb253a3e9aed606a4278e4954823f
Terry Tao <librakevin@gmail.com>
2014-08-20 16:58:40 +0800

    Refine mobile's show page

--- public/javascripts/mobile/layout.js
+++ public/javascripts/mobile/layout.js
@@ -1,19 +1,54 @@
-function toggleMenuBar(event) {
-    var display = $('.menu-bar').css('display');
+function toggleMainMenuBar(event) {
+    var display = $('.main-menu-bar').css('display');
     if (display == 'none') {
-        $('.menu-bar').fadeIn('slow', function() {
-            $('.menu-btn').css('background-image', 'url(/assets/images/menu_main_close.jpg)');
-        });
+        hideShowMenuBar();
+        showMainMenuBar();
     } else {
-        $('.menu-bar').fadeOut('slow', function() {
-            $('.menu-btn').css('background-image', 'url(/assets/images/menu_main.jpg)');
-        });
+        hideMainMenuBar();
     }
+};
+
+function showMainMenuBar() {
+    $('.main-menu-bar').fadeIn('slow', function() {
+        $('.main-menu-btn').css('background-image', 'url(/assets/images/menu_main_close.jpg)');
+    });
+};
+
+function hideMainMenuBar() {
+    $('.main-menu-bar').fadeOut('slow', function() {
+        $('.main-menu-btn').css('background-image', 'url(/assets/images/menu_main.jpg)');
+    });
+};
+
+function toggleShowMenuBar(event) {
+    var display = $('.show-menu-bar').css('display');
+    if (display == 'none') {
+        hideMainMenuBar();
+        showShowMenuBar();
+    } else {
+        hideShowMenuBar();
+    }
+};
+
+function showShowMenuBar() {
+    $('.show-menu-bar').fadeIn('slow', function() {
+        $('.show-menu-btn').css('background-image', 'url(/assets/images/menu_extra_close.jpg)');
+    });
+};
+
+function hideShowMenuBar() {
+    $('.show-menu-bar').fadeOut('slow', function() {
+        $('.show-menu-btn').css('background-image', 'url(/assets/images/menu_extra.jpg)');
+    });
 };
 
 $(document).ready(function() {
 
-    $('.menu-btn').click(function(event) {
-        toggleMenuBar(event);
+    $('.main-menu-btn').click(function(event) {
+        toggleMainMenuBar(event);
+    });
+
+    $('.show-menu-btn').click(function(event) {
+        toggleShowMenuBar(event);
     });
 });

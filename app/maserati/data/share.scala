d2446b7f313288a73eba01bb8fbca3275ed1b2b8
Terry Tao <librakevin@gmail.com>
2014-08-22 03:24:11 +0800

    Weibo share

---
+++ app/maserati/data/share.scala
@@ -0,0 +1,20 @@
+package maserati.data
+
+import java.net.URLEncoder
+
+object share {
+
+  val weiboUrl = {
+    val sinaShareLink = "http://service.weibo.com/share/share.php?"
+    val site = "http://360.iuseipad.com.cn"
+    val title = "#玛莎拉蒂# 传承意大利经典设计和手工定制传统，诠释优雅、奢华、运动和激情，玛莎拉蒂带您体验无与伦比的驾驶体验。" + site
+    val queryStrings = Map(
+      "appkey" -> "",
+      "title" -> title,
+      "url" -> site
+    )
+    sinaShareLink + queryStrings.map { case (k, v) => k + "=" + URLEncoder.encode(v, "UTF-8") }.mkString("&")
+  }
+
+  val email = "support@maserati.com"
+}

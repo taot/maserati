7d3acd0a67b04fd72dd59774b116dd7dff45f588
Huajie Wu <huajiewu@gmail.com>
2014-11-19 21:52:47 +0800

    wuhuajie: change by client

--- app/maserati/data/share.scala
+++ app/maserati/data/share.scala
@@ -7,7 +7,7 @@ object share {
   val weiboUrl = {
     val sinaShareLink = "http://service.weibo.com/share/share.php?"
     val site = "http://360.iuseipad.com.cn"
-    val title = "#玛莎拉蒂# 传承意大利经典设计和手工定制传统，诠释优雅、奢华、运动和激情，玛莎拉蒂带您体验无与伦比的驾驶体验。"
+    val title = "#玛莎拉蒂# 传承意大利经典设计和手工定制传统，诠释优雅、奢华、运动和激情，玛莎拉蒂带您体验无与伦比的驾驶体验。http://www.maseratiautoshow.cn/"
     val queryStrings = Map(
       "appkey" -> "",
       "title" -> title
